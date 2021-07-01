eb5852bd4106975d700a673ff1809ca867708907
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-07-01 09:00:24 +0530

    Update miko.py

--- miko.py
+++ miko.py
@@ -54,7 +54,11 @@ def return_time(user: int):
   hour = int(t/60)
   min = t%60
   return hour, min
-   
+
+def min_hour(time: int):
+  hour = int(time/60)
+  min = time%60
+  return hour, min
     
 @client.event
 async def on_ready():
@@ -87,14 +91,11 @@ async def help(ctx):
     )
     h.add_field(
       name="__COMMANDS__",
-      value=f"`start (time)` Starts the focus timer.\n`stop` Stops the focus timer.\n`total` Shows the total time you have focused.\n`inspire` Miko chan sends an inspirational quote for you.\n`padhle (user)` Just a normal study reminder for fun.",
+      value=f"`start (time)` Starts the focus timer.\n`stop` Stops the focus timer.\n`total` Shows the total time you have focused.\n`lb` Shows leaderboard of total focused time.\n`inspire` Miko chan sends an inspirational quote for you.\n`padhle (user)` Just a normal study reminder for fun.",
       inline=False
     )
     h.add_field(
       name="__WARNING__", value="If you are using focus timer, the time focused will be added only after you complete your focus time. Using `m.stop` will not work, So no cheating now, grind hard!", inline=False
-    )
-    h.add_field(
-      name="__MISC__", value="Miko Chan also replies to you when you say `I love you miko chan` or `I hate you miko chan`", inline=False
     )
     h.add_field(
       name="__SOURCE__", value="`m.source`", inline=False
@@ -156,8 +157,7 @@ async def start(ctx, time: int):
             title="Timer Ended!", description=f"⏱ Your focus time has ended ⏱\n\n😀 Take a break 😀", color=0xe81741)
             await ctx.send(ctx.author.mention, embed=emb)
             user_list.remove(ctx.author.id)
-            add_time(ctx.author.id, time)
-            
+            add_time(ctx.author.id, time)        
     else:
       return
   else:
@@ -165,7 +165,56 @@ async def start(ctx, time: int):
       title="", description=f"**You are already working!.**", color=0xe81741)
      await ctx.send(ctx.author.mention, embed=emb)
 
-       
+@client.command()
+async def lb(ctx):
+  lb_dict={}
+  desc = f""
+  guild = client.get_guild(GUILD_ID)
+  users_list=[]
+  all_users = db.child("USER_TIME").get()
+  for user in all_users:
+    user_id = user.key()
+    users_list.append(user_id)
+  for member in users_list:
+    time = db.child("USER_TIME").child(member).child("TOTAL").get().val()
+    lb_dict[member] = time
+  sort_lb = sorted(lb_dict.items(), key=lambda x: x[1], reverse=True)
+  for mem_id in sort_lb:
+    try:
+      member = await guild.fetch_member(mem_id)
+    except Exception:
+      member = "UNKNOWN MEMBER"
+    position = list(sort_lb.keys()).index(mem_id) + 1
+    hrs, mins = min_hour(sort_lb[mem_id])
+    if position > 10:
+      break
+    if mem_id == str(ctx.author.id):
+      desc += f"**{member} | {hrs} Hrs {mins} Mins**\n"
+    else:
+      if position == 1:
+        desc += f"👑 -> {member} | {hrs} Hrs {mins} Mins\n"
+      elif position == 10:  # ONE SPACE LESSER
+        desc += f"#{position}-> {member} | {hrs} Hrs {mins} Mins\n"
+      else:
+        desc += f"#{position} -> {member} | {hrs} Hrs {mins} Mins\n"
+
+  mem_id = str(ctx.author.id)
+  try:
+    member = await guild.fetch_member(mem_id)
+  except Exception:
+    member = "UNKNOWN MEMBER"
+  hrs, mins = min_hour(lb[mem_id])
+  position = list(lb.keys()).index(mem_id) + 1
+
+  emb = discord.Embed(
+    title=f"**TOTAL FOCUSED TIME LEADERBOARD**",
+    description=f"```\n{desc}\n```",
+    color=0xe81741,
+  )
+  emb.set_footer(text=f"#{position} -> {member} | {hrs} Hrs {mins} Mins")
+  await ctx.send(embed=emb)
+
+    
 @client.command()
 async def total(ctx):
   hour, minutes = return_time(ctx.author.id)
@@ -190,14 +239,12 @@ async def stop(ctx):
       pomodoro_timer = False
       emb = discord.Embed(
       title="", description=f"**⚠ Your timer has been stopped! ⚠**", color=0xe81741)
-      await ctx.send(ctx.author.mention)
-      await ctx.send(embed=emb)
+      await ctx.send(ctx.author.mention, embed=emb)
       user_list.remove(ctx.author.id)
     else:
       emb = discord.Embed(
       title="", description=f"**You are not currently working! Use m.start [time] to start a timer.**", color=0xe81741)
-      await ctx.send(ctx.author.mention)
-      await ctx.send(embed=emb) 
+      await ctx.send(ctx.author.mention, embed=emb) 
   else:
     return    
 
@@ -219,30 +266,18 @@ async def padhle(ctx, m1: discord.User = None):
 
 @client.event
 async def on_message(message):
-    await client.process_commands(message)
-
-    # Animal emoji reactions!
-    reactions = ['🐮', '🐷', '🐒', '🐼', '🐥', '🦕', '🐙']
-    if(emojisOn):
-        #for emoji in reactions: 
-        emoji = random.choice(reactions)
-        await message.add_reaction(emoji)
-
-    if message.author == client.user:
-        return
-
-    
-    if message.author.id in user_list:
-        if message.channel.id == CAFE_LOUNGE_ID:
-          if message.content.startswith('m.') or message.content.startswith('s.') or message.content.startswith('S.'):
-            return
-          else:
-            if message.author.id == 534384083925598218:
-              await message.channel.send(f'{message.author.mention}\n**Onii Chan Baka..** If you talk during your focus time I will kill you🔪..', delete_after=15)
-            else:
-              await message.channel.send(f'{message.author.mention}\n**Onii Chan Baka..** If you talk during your focus time I will not love you😡..', delete_after=15)
+  await client.process_commands(message)
+  if message.author.id in user_list:
+    if message.channel.id == CAFE_LOUNGE_ID:
+      if message.content.startswith('m.') or message.content.startswith('s.') or message.content.startswith('S.'):
+          return
+      else:
+        if message.author.id == 534384083925598218:
+          await message.channel.send(f'{message.author.mention}\n**Onii Chan Baka..** If you talk during your focus time I will kill you🔪..', delete_after=15)
         else:
-            return
+          await message.channel.send(f'{message.author.mention}\n**Onii Chan Baka..** If you talk during your focus time I will not love you😡..', delete_after=15)
+    else:
+      return
 
 
 token = config("TOKEN")
